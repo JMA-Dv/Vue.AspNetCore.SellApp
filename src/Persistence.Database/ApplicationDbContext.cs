d77b8abe93b556361ced0226c2a9efac0ec792c4
Jesus Mata <alejandroavila98@hotmail.com>
2022-03-09 00:29:13 -0600

    Code base

---
+++ src/Persistence.Database/ApplicationDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Model;
+using Persistence.Database.Config;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Persistence.Database
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) :base(options)
+        {
+        }
+        public DbSet<Product> Products{ get; set; }
+
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+        public DbSet<Client> Clients { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            new ClientConfig(modelBuilder.Entity<Client>());
+            new ProductConfig(modelBuilder.Entity<Product>());
+        }
+
+    }
+}
